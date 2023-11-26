2397cd1461ae1abd12ed10be685cba9c9ad47876
gaurav <gauravpathak9058@gmail.com>
2023-11-26 18:41:28 +0530

    firsth

--- src/components/movieCard/MovieCard.jsx
+++ src/components/movieCard/MovieCard.jsx
@@ -7,6 +7,7 @@ import "./style.scss";
 import Img from "../lazyLoadImage/Img";
 import CircleRating from "../circleRating/CircleRating";
 
+
 import Genres from "../genres/Genres";
 import PosterFallback from "../../assets/no-poster.png";
 
